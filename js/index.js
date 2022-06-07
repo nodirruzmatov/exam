017a5a64e86dceb19514b3257f8311dc8e9adfc2
nodirruzmatov <nodir7ruzmatov@gmail.com>
2022-06-07 11:08:02 +0300

    feat: in the process of adding cards

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const elPokemonList = document.querySelector("pokemon__list");
+
+for (let pokemon of pokemons) {
+  // ! CREATE ELEMENTS
+  const newLI = document.createElement("li");
+  const newDivCard = document.createElement("div");
+  const newImg = document.createElement("img");
+  const newDivCardBody = document.createElement("div");
+  const newPokemonName = document.createElement("h5");
+  const newPokemonType = document.createElement("p");
+  const newPokemonAge = document.createElement("p");
+  const newPokemonWeight = document.createElement("p");
+
+  // !SET ATTTIBUTE
+  newLI.setAttribute("");
+}
